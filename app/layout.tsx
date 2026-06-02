f00d7b4832ae8d45dd1dd413495d1d9b477ceb5e
Tangguh Riyadi <mtangguh97@gmail.com>
2026-06-02 19:48:00 +0700

    add analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Inter, Geist } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -64,7 +65,10 @@ export default function RootLayout({
       lang="en"
       className={`${montserrat.variable} ${inter.variable} ${geist.variable}`}
     >
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
